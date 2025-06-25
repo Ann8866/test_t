2ae7f3566d218cc5cfccf304f1f95c1178470806
Ann <knopa8866gmail.com>
2025-06-25 13:45:28 +0300

    Коммит

---
+++ example.py
@@ -0,0 +1,22 @@
+a = 10
+b = a + 20
+f = b
+if f > 10:
+    print("Больше 10")
+else:
+    print("10 или меньше")
+
+
+# x = int(input())
+# if -1 < x < 17:
+#     print('Принадлежит')
+# else:
+#     print('Не принадлежит')
+#
+# a = int(input())
+# b = int(input())
+# c= int(input())
+# if a + b > c and a + c > b and b + c > a:
+#     print('YES')
+# else:
+#     print('NO')
